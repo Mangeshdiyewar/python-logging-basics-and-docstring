65dcf4eb1a265d50e41201cef278f15196465440
Mangeshdiyewar <mangeshmjd@gmail.com>
2022-10-25 19:21:38 +0530

    logging added for or model

--- and.py
+++ and.py
@@ -1,10 +1,22 @@
 import pandas as pd
 from  utlis.all_utils import prepare_data , save_plot
 from  utlis.model import Perceptron
+import logging
+import os
 
+gate = "AND gate"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(
+    filename=os.path.join(log_dir,"running_logs.log"),
+    level= logging.INFO,
+    format='[%(asctime)s:%(levelname)s:%(module)s]:%(message)s',
+    filemode='a'
+    )
 def main(data,modelName,plotName,eta,epochs):
-    df_AND = pd.DataFrame(data)
-    x, y = prepare_data(df_AND)
+    df= pd.DataFrame(data)
+    logging.info('this is raw data set:\n{df}')
+    x, y = prepare_data(df)
 
     model_AND = Perceptron(eta=eta, epochs=epochs)
     model_AND.fit(x, y)
@@ -13,7 +25,7 @@ def main(data,modelName,plotName,eta,epochs):
 
     model_AND.save(filename=modelName, model_dir="model_AND")
 
-    save_plot(df_AND, model_AND, filename=plotName)
+    save_plot(df, model_AND, filename=plotName)
 
 
 if __name__ == "__main__":
@@ -27,6 +39,14 @@ if __name__ == "__main__":
   }
   ETA =0.3
   EPOCHS =10
-  main(data=AND, modelName="and_model",plotName= "and.png", eta =ETA,epochs =EPOCHS)
 
+  try:
+
+     logging.info(">>>>> starting training {gate} >>>>")
+     main(data=AND, modelName="and_model",plotName= "and.png", eta =ETA,epochs =EPOCHS)
+     logging.info(f"<<<<<done training for {gate}<<<<<\n\n")
+
+  except Exception as e:
+     logging.exception(e)
+     raise e
 
